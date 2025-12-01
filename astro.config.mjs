0532e8e8d6a6db35989b02fd8bfe0ea6fc981063
Jose Baeza <josedelarosabass@gmail.com>
2025-12-01 11:33:17 -0600

    feat: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,27 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@astrojs/preact';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [preact()],
-  
+
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'it'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
